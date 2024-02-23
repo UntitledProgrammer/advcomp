e4732e4b14ab88fb51f6d34d98c8ab81574b4dd3
Thomas Jacobs <72556396+UntitledProgrammer@users.noreply.github.com>
2024-02-23 11:13:58 +0000

    Update.

--- notes/__init__.py
+++ notes/__init__.py
@@ -1,9 +1,17 @@
 import sqlite3
-import constants
+from . import constants
 import logging
-from note_structure import Note
-from sql_result import SqlResult
+from .note_structure import Note
+from .sql_result import SqlResult
 import datetime
+
+
+# For testing only!
+def print_all() -> None:
+    rows: list = __get_connection().execute(f"SELECT * FROM {constants.NOTES_TABLE_NAME};")
+
+    for row in rows:
+        print(row)
 
 
 def __get_connection() -> sqlite3.Connection:
@@ -20,6 +28,9 @@ def __get_connection() -> sqlite3.Connection:
         # Ensure the notes database table is initialised.
         connection.execute(f'CREATE TABLE IF NOT EXISTS {constants.NOTES_TABLE_NAME} (id INTEGER PRIMARY KEY AUTOINCREMENT, username TEXT NOT NULL, title TEXT, description TEXT, date TEXT);')
 
+        # Return the SQLite database connection.
+        return connection
+
     except sqlite3.Error as error:
 
         # Log the error for future record.
@@ -30,7 +41,7 @@ def __get_connection() -> sqlite3.Connection:
 
 
 def __get_date_now() -> str:
-    return datetime.now.strftime("%m/%d/%Y, %H:%M:%S")
+    return datetime.datetime.now().strftime("%m/%d/%Y %H:%M:%S")
 
 
 def add_note(note: Note) -> bool:
@@ -81,14 +92,14 @@ def remove_note(identification: int) -> bool:
         raise error
 
 
-def update_note(identification: int, note: Note) -> None:
+def update_note(identification: int, title: str, description: str) -> None:
     try:
         # Initialise an SQLite database connection.
         connection: sqlite3.Connection = __get_connection()
 
         # Separate the query and values to prevent SQL injection.
         query: str = f"UPDATE {constants.NOTES_TABLE_NAME} SET title = ?, description = ?, date = ? WHERE id = ?;"
-        connection.execute(query, (note.title, note.description, __get_date_now(),))
+        connection.execute(query, (title, description, __get_date_now(), identification,))
 
         # Save and close the database.
         connection.commit()
